80999ac1bc1ea2018050319fe763584953620685
sacide <157601350+Sacidea@users.noreply.github.com>
2026-06-09 15:42:54 +0300

    chore: configure vitest to ignore playwright tests

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   test: {
     environment: 'node', // Change to jsdom or happy-dom if UI components are tested
     globals: true,
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'tests/**'],
   },
 });
